8bdcb1ed3b799e9afa40663b022983b48db6c740
Allen Rohner <arohner@gmail.com>
2015-07-27 15:12:04 -0500

    New approach, download on first run, which makes it legal to distribute

--- src/leiningen/dynamodb_local.clj
+++ src/leiningen/dynamodb_local.clj
@@ -1,28 +1,43 @@
 (ns leiningen.dynamodb-local
   (:require [clojure.java.io :as io]
             [leiningen.core.main :as main])
-  (:import [java.nio.file Files]
+  (:import [java.nio.file Files Paths LinkOption Path]
            [java.nio.file.attribute FileAttribute]
            [org.apache.commons.io FileUtils]
            [net.lingala.zip4j.core ZipFile]))
 
-(def ^:private dynamodb-local-version "dynamodb_local_2014-04-24")
+(def download-url "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.zip")
 
-(def ^:private temp-directory
-  (delay (-> (Files/createTempDirectory "lein-dynamodb-local" (make-array FileAttribute 0))
-             (.toString))))
+(def dynamo-directory "lein-dynamodb-local")
+
+(defn ->path [str & strs]
+  {:pre [(string? str) (every? string? strs)]}
+  (Paths/get str (into-array String strs)))
+
+(defn path? [x]
+  (instance? Path x))
+
+(defn exists? [path]
+  {:pre [(path? path)]}
+  (Files/exists path (into-array LinkOption [])))
+
+(defn ensure-dynamo-directory []
+  (let [path (->path dynamo-directory)]
+    (when-not (exists? path)
+      (-> (Files/createDirectory path (make-array FileAttribute 0))
+          (.toString)))))
 
 (defn- start-dynamo
   "Start DynamoDB Local with the given options"
-  [dir port in-memory? db-path]
-  (let [lib-path (str (io/file dir dynamodb-local-version "DynamoDBLocal_lib"))
-        jar-path (str (io/file dir dynamodb-local-version "DynamoDBLocal.jar"))
+  [port in-memory? db-path]
+  (let [lib-path (str (io/file dynamo-directory "DynamoDBLocal_lib"))
+        jar-path (str (io/file dynamo-directory "DynamoDBLocal.jar"))
         command (cond-> (format "java -Djava.library.path=%s -jar %s -port %s" lib-path jar-path port)
                         in-memory? (str " -inMemory")
                         (and (seq db-path) (not in-memory?)) (str " -dbPath " db-path))]
     (.exec (Runtime/getRuntime) command)))
 
-(defn- dynamo-options
+(defn dynamo-options
   "Use DynamoDB Local options provided or default values"
   [project]
   (merge {:port 8000
@@ -30,29 +45,35 @@
           :db-path "."}
          (:dynamodb-local project)))
 
-(defn- unpack-dynamo
-  "Unpack the DynamoDB Local jar and libraries to the specified directory"
-  [dir]
-  (let [zip-file (str dynamodb-local-version ".zip")
-        temp-zip (io/file dir zip-file)]
-    (with-open [zip-stream (.openStream (io/resource zip-file))]
-      (io/copy zip-stream temp-zip))
-    (.extractAll (ZipFile. temp-zip) dir)))
+(defn download-dynamo [url]
+  (println "downloading dynamo")
+  (ensure-dynamo-directory)
+  (io/copy (io/input-stream (io/as-url url)) (io/as-file (str dynamo-directory "/" "dynamo.zip"))))
 
-(defn- clean-up-on-shutdown
-  "Delete the temp directory and kill the DynamoDB Local process on JVM shutdown"
-  [dir dynamo-process]
+(defn unpack-dynamo []
+  (println "unpacking dynamo")
+  (let [zip-file (->path dynamo-directory "dynamo.zip")]
+    (.extractAll (ZipFile. (str zip-file)) dynamo-directory)))
+
+(defn ensure-installed []
+  (when-not (exists? (->path dynamo-directory "dynamo.zip"))
+    (download-dynamo download-url)
+    (unpack-dynamo)))
+
+(defn clean-up-on-shutdown
+  "kill the DynamoDB Local process on JVM shutdown"
+  [dynamo-process]
   (.addShutdownHook (Runtime/getRuntime)
-                    (Thread. (fn [] (doto dynamo-process (.destroy) (.waitFor))
-                               (FileUtils/deleteDirectory (io/file dir))))))
+                    (Thread. (fn [] (doto dynamo-process (.destroy) (.waitFor))))))
 
 (defn dynamodb-local
   "Run DynamoDB Local for the lifetime of the given task"
   [project & args]
-  (unpack-dynamo @temp-directory)
+  (ensure-installed)
   (let [{:keys [port in-memory? db-path]} (dynamo-options project)
-        dynamo-process (start-dynamo @temp-directory port in-memory? db-path)]
-    (clean-up-on-shutdown @temp-directory dynamo-process)
+        dynamo-process (start-dynamo port in-memory? db-path)]
+    (println "started dynamo")
+    (clean-up-on-shutdown dynamo-process)
     (if (seq args)
       (main/apply-task (first args) project (rest args))
       (while true (Thread/sleep 5000)))))
