b4dd01c63280d36b70bba35286103e78449dfd24
Allen Rohner <arohner@gmail.com>
2015-07-27 15:08:23 -0500

    lein-release plugin: preparing 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-dynamodb-local "0.1.0-SNAPSHOT"
+(defproject arohner/lein-dynamodb-local "0.2.0"
   :description "A Leiningen plugin for providing a local DynamoDB instance"
   :url "https://github.com/dmcgillen/lein-dynamodb-local"
   :license {:name "Eclipse Public License"
@@ -7,5 +7,5 @@
   :dependencies [[commons-io/commons-io "2.4"]
                  [net.lingala.zip4j/zip4j "1.3.2"]]
   :plugins [[lein-release "1.0.5"]]
-  :lein-release {:deploy-via :clojars
-                 :clojars-url "clojars@clojars.org:"})
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]])
