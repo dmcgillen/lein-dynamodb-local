ea64b6f7eb869b039b977d5a9fc4145595dd9dce
Donovan McGillen <donovan.mcgillen@microsoft.com>
2015-07-28 21:01:32 +0100

    Version 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-dynamodb-local "0.2.4-SNAPSHOT"
+(defproject lein-dynamodb-local "0.2.4"
   :description "A Leiningen plugin for providing a local DynamoDB instance"
   :url "https://github.com/dmcgillen/lein-dynamodb-local"
   :license {:name "Eclipse Public License"
